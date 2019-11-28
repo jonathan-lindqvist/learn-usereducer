7dc3c94c16192bad65eda1c3cf8c0a3f48f22fdb
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2019-11-28 16:33:09 +0100

    Counter clicker with useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import './App.css';
 
+
+const reducer = (state:number, action:any) => {
+  switch(action.type){
+    case 'INC':
+      return state + 1
+    case 'DEC':
+      return state - 1
+    default:
+      return state
+  }
+}
+
 const App: React.FC = () => {
+  const [count, dispatch] = useReducer(reducer, 0)
+
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <div>Count: {count}</div>
+      <button onClick={() => dispatch({type: 'INC'})}>Increment</button>
+      <button onClick={() => dispatch({type: 'DEC'})}>Decrement</button>
     </div>
   );
 }
